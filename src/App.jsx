2555f570c1564d5e73e0ed7c6eda3d465a4b18a9
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-08-24 11:19:11 +0200

    adds info about the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Link,BrowserRouter} from 'react-router-dom';
+import {Link,BrowserRouter,Switch,Route} from 'react-router-dom';
 import Header from "./Header"
-
+import Hello from "./Hello"
 
 function App() {
   return (
     <div >
       <BrowserRouter>
        <Header />
-      
+       <Switch>
+        <Route exact path='/' component={Hello}></Route>
+        
+      </Switch> 
        </BrowserRouter>
     </div>
   );
---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Hello = () => {
+  return (
+      <div className="ui main container">
+      <p>It asks you a question, you get four choices as an answer,answer it!!!! </p>
+    </div>  
+  )
+}
+
+export default Hello

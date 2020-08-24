a80e1d07f375f48faaf0acbae56fa076c8cada44
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-08-24 11:21:22 +0200

    adds a new file footer

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+const Footer = () => {
+return(
+    <footer>
+      <div >
+      <button> <NavLink className="ui item" activeStyle={{ fontWeight: "bold" }} to='/start'>START</NavLink></button>  
+      </div>
+    </footer>
+  )
+}
+export default Footer
--- src/Header.jsx
+++ src/Header.jsx
@@ -6,9 +6,6 @@ const Header = () => {
     <nav className='ui fixed inverted menu'>
       <div className="ui container">
         <Link className="header item" to='/'>QUIZE</Link>
-        <div className="right menu">
-         <button> <NavLink className="ui item" activeStyle={{ fontWeight: "bold" }} to='/start'>START</NavLink></button>
-        </div>
       </div>
     </nav>
     )
